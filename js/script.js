1046f1146b78408c7872f0fd27df49fff205ae6a
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-07-14 14:36:31 +0300

    commit9

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function () {
 
 function getWeather(city) {
   const apiKey = "b429d88ed86c4d4996a202219241307";
-  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`; // Modificat la HTTPS
+  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`; // Utilizăm HTTPS
   $.ajax({
     url: apiUrl,
     method: "GET",
@@ -36,7 +36,7 @@ function getWeather(city) {
 
 function getWeatherByCoordinates(lat, lon) {
   const apiKey = "b429d88ed86c4d4996a202219241307";
-  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3`; // Modificat la HTTPS
+  const apiUrl = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${lat},${lon}&days=3`; // Utilizăm HTTPS
   $.ajax({
     url: apiUrl,
     method: "GET",
